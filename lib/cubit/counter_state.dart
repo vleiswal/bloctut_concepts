054ebf68f97f19f5d2e143763083398ef8686514
vleiswal <vleiswal@gmail.com>
2021-01-22 04:56:14 +0200

    working

---
+++ lib/cubit/counter_state.dart
@@ -0,0 +1,11 @@
+part of 'counter_cubit.dart';
+
+class CounterState {
+  int counterValue;
+
+  CounterState({
+    @required this.counterValue,
+  });
+}
+
+class CounterInitial extends CounterState {}
