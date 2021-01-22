204a464b425d161ced50ac0f5af85542dc5d071e
Vleis Walker <vleiswal@gmail.com>
2021-01-22 20:14:28 +0200

    finished

--- lib/main.dart
+++ lib/main.dart
@@ -33,167 +33,79 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  // int _counter = 0;
-
-  // void _incrementCounter() {
-  //   setState(() {
-  //     _counter++;
-  //   });
-  // }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
       ),
-      body: BlocListener<CounterCubit, CounterState>(
-        listener: (context, state) {
-          if (state.wasIncremented) {
-            // do increment SnackBar
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Increment'),
-                duration: Duration(milliseconds: 1000),
-              ),
-            );
-          } else if (!state.wasIncremented) {
-            // do decrement SnackBar
-            ScaffoldMessenger.of(context).showSnackBar(
-              SnackBar(
-                content: Text('Decrement'),
-                duration: Duration(milliseconds: 1000),
-              ),
-            );
-          }
-        },
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'You have pushed the button this many times:',
-              ),
-              SizedBox(height: 20),
-              BlocBuilder<CounterCubit, CounterState>(
-                builder: (context, state) {
-                  var message = '';
-                  if (state.counterValue == 0) {
-                    message = 'Zero ';
-                  } else if (state.counterValue == 5) {
-                    message = 'Ahhhh ';
-                  } else if (state.counterValue < 0) {
-                    message = 'Minus ';
-                  }
-
-                  return Text(
-                    message + state.counterValue.toString(),
-                    style: Theme.of(context).textTheme.headline4,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'You have pushed the button this many times:',
+            ),
+            SizedBox(height: 20),
+            BlocConsumer<CounterCubit, CounterState>(
+              listener: (context, state) {
+                if (state.wasIncremented) {
+                  // do increment SnackBar
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Increment'),
+                      duration: Duration(milliseconds: 1000),
+                    ),
                   );
-                },
-              ),
-              SizedBox(height: 20),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  FloatingActionButton(
-                    onPressed: () {
-                      BlocProvider.of<CounterCubit>(context).decrement();
-                    },
-                    tooltip: 'Decrement',
-                    child: Icon(Icons.remove),
-                  ),
-                  FloatingActionButton(
-                    onPressed: () {
-                      BlocProvider.of<CounterCubit>(context).increment();
-                    },
-                    tooltip: 'Increment',
-                    child: Icon(Icons.add),
-                  ),
-                ],
-              ),
-            ],
-          ),
+                } else if (!state.wasIncremented) {
+                  // do decrement SnackBar
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('Decrement'),
+                      duration: Duration(milliseconds: 1000),
+                    ),
+                  );
+                }
+              },
+              builder: (context, state) {
+                var message = '';
+                if (state.counterValue == 0) {
+                  message = 'Zero ';
+                } else if (state.counterValue == 5) {
+                  message = 'Ahhhh ';
+                } else if (state.counterValue < 0) {
+                  message = 'Minus ';
+                }
+
+                return Text(
+                  message + state.counterValue.toString(),
+                  style: Theme.of(context).textTheme.headline4,
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).decrement();
+                  },
+                  tooltip: 'Decrement',
+                  child: Icon(Icons.remove),
+                ),
+                FloatingActionButton(
+                  onPressed: () {
+                    BlocProvider.of<CounterCubit>(context).increment();
+                  },
+                  tooltip: 'Increment',
+                  child: Icon(Icons.add),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 }
-
-// @override
-// Widget build(BuildContext context) {
-//   return Scaffold(
-//     appBar: AppBar(
-//       title: Text(widget.title),
-//     ),
-//     body: BlocListener<CounterCubit, CounterState>(
-//       listener: (context, state) {
-//         if (state.wasIncremented) {
-//           // do increment SnackBar
-//           ScaffoldMessenger.of(context).showSnackBar(
-//             SnackBar(
-//               content: Text('Increment'),
-//               duration: Duration(milliseconds: 1000),
-//             ),
-//           );
-//         } else if (!state.wasIncremented) {
-//           // do decrement SnackBar
-//           ScaffoldMessenger.of(context).showSnackBar(
-//             SnackBar(
-//               content: Text('Decrement'),
-//               duration: Duration(milliseconds: 1000),
-//             ),
-//           );
-//         }
-//       },
-//       child: Center(
-//         child: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: <Widget>[
-//             Text(
-//               'You have pushed the button this many times:',
-//             ),
-//             SizedBox(height: 20),
-//             BlocBuilder<CounterCubit, CounterState>(
-//               builder: (context, state) {
-//                 var message = '';
-//                 if (state.counterValue == 0) {
-//                   message = 'Zero ';
-//                 } else if (state.counterValue == 5) {
-//                   message = 'Ahhhh ';
-//                 } else if (state.counterValue < 0) {
-//                   message = 'Minus ';
-//                 }
-
-//                 return Text(
-//                   message + state.counterValue.toString(),
-//                   style: Theme.of(context).textTheme.headline4,
-//                 );
-//               },
-//             ),
-//             SizedBox(height: 20),
-//             Row(
-//               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-//               children: <Widget>[
-//                 FloatingActionButton(
-//                   onPressed: () {
-//                     BlocProvider.of<CounterCubit>(context).decrement();
-//                   },
-//                   tooltip: 'Decrement',
-//                   child: Icon(Icons.remove),
-//                 ),
-//                 FloatingActionButton(
-//                   onPressed: () {
-//                     BlocProvider.of<CounterCubit>(context).increment();
-//                   },
-//                   tooltip: 'Increment',
-//                   child: Icon(Icons.add),
-//                 ),
-//               ],
-//             ),
-//           ],
-//         ),
-//       ),
-//     ),
-//   );
-// }
